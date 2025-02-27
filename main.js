462a6eb786c613b60d25eef4cc1e251875381d0a
Elaine Sajets <elainesajets@gmail.com>
2025-02-27 17:26:46 +0900

    Added prompt where user can change the sketch pad grid size

--- main.js
+++ main.js
@@ -1,31 +1,66 @@
 const container = document.getElementById('container');
 const resetButton = document.getElementById('reset-button');
+const gridSizeButton = document.getElementById('grid-size');
 
-do {
-  const square = document.createElement('div');
-  let squareId = container.childElementCount;
+let columns = 16;
 
-  square.textContent = '';
+// Initial grid setup
 
-  square.setAttribute('id', squareId + 1);
-  square.classList.add('square');
-  container.appendChild(square);
+function createGrid(numColumns) {
+  for (let i = 0; i < numColumns * numColumns; i++) {
+    const square = document.createElement('div');
+    let squareId = container.childElementCount;
 
-  square.addEventListener('mouseover', (e) => {
-    e.target.classList.add('square-hover');
-  });
-} while (container.childElementCount < 256);
+    let squareSize = (1 / numColumns) * 100;
+    console.log(squareSize);
+
+    square.setAttribute('id', squareId + 1);
+    square.style.width = squareSize + '%';
+    square.style.height = squareSize + '%';
+    square.style.flexBasis = squareSize + '%';
+    square.classList.add('square');
+    container.appendChild(square);
+
+    square.addEventListener('mouseover', (e) => {
+      e.target.classList.add('square-hover');
+    });
+  }
+}
+
+createGrid(columns);
+
+gridSizeButton.addEventListener('click', () => {
+  let userColumns = Number(prompt('Chose a number of columns! (Maximum 100)'));
+
+  if (isNaN(userColumns) || !userColumns) {
+    alert('Not a valid number. Try again!');
+    return;
+  }
+
+  if (userColumns >= 101) {
+    alert(`That's too high.\n Enter a number less than or equal to 100.  `);
+    return;
+  }
+
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  createGrid(userColumns);
+});
+
+function resetSketchPad() {
+  const squares = document.querySelectorAll('.square');
+  for (let square of squares) {
+    square.classList.remove('square-hover');
+  }
+}
 
 resetButton.addEventListener('click', () => {
-  const squares = document.querySelectorAll('.square');
-
   container.classList.add('tilt-shaking');
+  resetSketchPad();
 
   container.addEventListener('animationend', () => {
     container.classList.remove('tilt-shaking');
   });
-
-  for (let square of squares) {
-    square.classList.remove('square-hover');
-  }
 });
